37a20d3bfc0c2c21a0fee5d151fac7a384f64f83
Wei Zhao <weizhao@vt.edu>
2021-12-29 14:31:36 -0600

    Update test_mesh_quad8.m

--- VAT_Plate/SPAD_VAT/subroutines/mesh/test_mesh_quad8.m
+++ VAT_Plate/SPAD_VAT/subroutines/mesh/test_mesh_quad8.m
@@ -3,12 +3,10 @@ clc;
 global FEM
 % generate 8-noded CQUAD8 for a unit square panel
 
-% 1) the current code works for even number element, will update it for all elements
-% number, sorry for inconvenience.
 
 
-xmesh_size = 8; % use even value
-ymesh_size = 8; % use even value
+xmesh_size = 7; %  
+ymesh_size = 7; %  
 
 % generate mesh
 FEM = mesh_QUAD8_v2(xmesh_size,ymesh_size);
